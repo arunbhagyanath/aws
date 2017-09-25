f66e688318c5a64d7826b3655daa46bcddc180bb
Arun Bhagyanath <arunbhagyanath@users.noreply.github.com>
2017-09-25 22:14:34 +0530

    Revert "Remove private on the EC2 library"
    This reverts commit 1c063cf219adbd049b9ea095b78bf48bc6441985.

--- libraries/ec2.rb
+++ libraries/ec2.rb
@@ -62,6 +62,8 @@ module AwsCookbook
       end
     end
 
+    private
+
     # setup AWS instance using passed creds, iam profile, or assumed role
     def create_aws_interface(aws_interface, **opts)
       aws_interface_opts = { region: opts[:region] }
