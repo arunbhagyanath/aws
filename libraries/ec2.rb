571b91fec99353989b4c7e43971d501e63cf6b96
Arun Bhagyanath <arunbhagyanath@users.noreply.github.com>
2017-09-25 22:14:34 +0530

    Revert "Fix EC2 client loading"
    This reverts commit 83bea9a29748d01bd94d86c71f46a0212f59405f.

--- libraries/ec2.rb
+++ libraries/ec2.rb
@@ -14,11 +14,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+require File.join(File.dirname(__FILE__), 'aws')
 require 'open-uri'
 
 module Opscode
   module Aws
     module Ec2
+      include Opscode::Aws
+
       def ec2
         require 'aws-sdk'
 
