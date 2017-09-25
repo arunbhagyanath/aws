55db3943e6fdf0d51129643e0cafae5707e34edf
Arun Bhagyanath <arunbhagyanath@users.noreply.github.com>
2017-09-25 22:14:34 +0530

    Revert "Rely on converge_by for the logging"
    This reverts commit 3a13c88b0b73ff0b00d56c2fd88afb5845c14d62.

--- resources/instance_monitoring.rb
+++ resources/instance_monitoring.rb
@@ -6,8 +6,11 @@ property :aws_role_session_name, String
 property :region, String
 
 action :enable do
-  unless monitoring_enabled?
+  if monitoring_enabled?
+    Chef::Log.debug('Monitoring is already enabled for this instance')
+  else
     converge_by('enable monitoring for this instance') do
+      Chef::Log.info('Enabling monitoring for this instance')
       ec2.monitor_instances(instance_ids: [node['ec2']['instance_id']])
     end
   end
@@ -16,8 +19,11 @@ end
 action :disable do
   if monitoring_enabled?
     converge_by('disable monitoring for this instance') do
+      Chef::Log.info('Disabling monitoring for this instance')
       ec2.unmonitor_instances(instance_ids: [node['ec2']['instance_id']])
     end
+  else
+    Chef::Log.debug('Monitoring is already disabled for this instance')
   end
 end
 
